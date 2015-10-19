7cb16af9228c4ea0cc3adea3353bb115d078b296
JackSparrow1313 <jacksparrow1313@mail.ru>
2015-10-19 07:14:07 +0300

    Homework

---
+++ 18.10.15/Repeat.java
@@ -0,0 +1,14 @@
+public class Repeat{
+	public static void main (String[] args){
+		String a = args[0];
+		boolean c = true;
+		for (int i=0; i < a.length() - 1; i++){
+			if (a.charAt(i) == a.charAt(i + 1)){
+				System.out.print("Repeat " + a.charAt(i));
+				c = false;
+				break;
+			}			
+		}
+		if (c) System.out.print("No repeat");
+	}
+}
